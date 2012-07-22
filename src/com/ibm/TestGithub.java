78f68cf70f2d5c8bf65f617022349283698dcc53
cgm_1521 <cgm_1521@163.com>
2012-07-22 11:19:09 +0800

    TestGithub

---
+++ src/com/ibm/TestGithub.java
@@ -0,0 +1,18 @@
+package com.ibm;
+
+/**
+ * ≤‚ ‘github
+ * @author CuiGuiming
+ *
+ */
+public class TestGithub {
+
+	/**
+	 * main∑Ω∑®
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test GitHub...");
+	}
+
+}
